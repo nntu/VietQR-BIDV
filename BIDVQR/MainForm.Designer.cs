a1a6ff5e5d7c7cfb90c7a63a52faf1457ec6bd36
ngoctuct-home <ngoctuct@gmail.com>
2022-04-27 22:21:31 +0700

    khởi tạo project BIDVQR

--- BIDVQR/MainForm.Designer.cs
+++ BIDVQR/MainForm.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace VietQR
+﻿namespace BIDVQR
 {
     partial class MainForm
     {
@@ -29,9 +28,7 @@ namespace VietQR
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -43,44 +40,36 @@ namespace VietQR
             this.bt_taoma = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.tb_tieude = new System.Windows.Forms.TextBox();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.tieuDeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hoTenDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.soTkDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataExcelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.bt_xuatFilePdf = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.bt_LoadExcel = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataExcelBindingSource = new System.Windows.Forms.BindingSource();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.tabControl1.SuspendLayout();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage2.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataExcelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataExcelBindingSource)).BeginInit();
+            this.tabPage1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // toolStripProgressBar1
             // 
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 450);
-            this.tabControl1.TabIndex = 0;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
             // 
             // tabPage2
             // 
@@ -89,7 +78,7 @@ namespace VietQR
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.Size = new System.Drawing.Size(792, 402);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Tao QRcode";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -170,7 +159,6 @@ namespace VietQR
             this.bt_taoma.TabIndex = 3;
             this.bt_taoma.Text = "Tạo QRCode";
             this.bt_taoma.UseVisualStyleBackColor = true;
-            this.bt_taoma.Click += new System.EventHandler(this.bt_taoma_Click);
             // 
             // label3
             // 
@@ -188,16 +176,49 @@ namespace VietQR
             this.tb_tieude.Size = new System.Drawing.Size(247, 20);
             this.tb_tieude.TabIndex = 2;
             // 
-            // tabPage1
+            // openFileDialog1
             // 
-            this.tabPage1.Controls.Add(this.splitContainer1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 424);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Tạo Theo DS";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel File|*.xlsx";
+            // 
+            // bt_xuatFilePdf
+            // 
+            this.bt_xuatFilePdf.Location = new System.Drawing.Point(22, 91);
+            this.bt_xuatFilePdf.Name = "bt_xuatFilePdf";
+            this.bt_xuatFilePdf.Size = new System.Drawing.Size(113, 23);
+            this.bt_xuatFilePdf.TabIndex = 2;
+            this.bt_xuatFilePdf.Text = "Xuất File PDF";
+            this.bt_xuatFilePdf.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(19, 21);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(71, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Lấy Template";
+            // 
+            // bt_LoadExcel
+            // 
+            this.bt_LoadExcel.Location = new System.Drawing.Point(22, 47);
+            this.bt_LoadExcel.Name = "bt_LoadExcel";
+            this.bt_LoadExcel.Size = new System.Drawing.Size(132, 23);
+            this.bt_LoadExcel.TabIndex = 0;
+            this.bt_LoadExcel.Text = "Load Template";
+            this.bt_LoadExcel.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.dataExcelBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(504, 396);
+            this.dataGridView1.TabIndex = 0;
             // 
             // splitContainer1
             // 
@@ -214,82 +235,20 @@ namespace VietQR
             this.splitContainer1.Panel2.Controls.Add(this.bt_xuatFilePdf);
             this.splitContainer1.Panel2.Controls.Add(this.linkLabel1);
             this.splitContainer1.Panel2.Controls.Add(this.bt_LoadExcel);
-            this.splitContainer1.Size = new System.Drawing.Size(786, 418);
+            this.splitContainer1.Size = new System.Drawing.Size(786, 396);
             this.splitContainer1.SplitterDistance = 504;
             this.splitContainer1.TabIndex = 0;
             // 
-            // dataGridView1
+            // tabPage1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.tieuDeDataGridViewTextBoxColumn,
-            this.hoTenDataGridViewTextBoxColumn,
-            this.soTkDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.dataExcelBindingSource;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(504, 418);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // tieuDeDataGridViewTextBoxColumn
-            // 
-            this.tieuDeDataGridViewTextBoxColumn.DataPropertyName = "Tieu_De";
-            this.tieuDeDataGridViewTextBoxColumn.HeaderText = "Tieu_De";
-            this.tieuDeDataGridViewTextBoxColumn.Name = "tieuDeDataGridViewTextBoxColumn";
-            // 
-            // hoTenDataGridViewTextBoxColumn
-            // 
-            this.hoTenDataGridViewTextBoxColumn.DataPropertyName = "HoTen";
-            this.hoTenDataGridViewTextBoxColumn.HeaderText = "HoTen";
-            this.hoTenDataGridViewTextBoxColumn.Name = "hoTenDataGridViewTextBoxColumn";
-            // 
-            // soTkDataGridViewTextBoxColumn
-            // 
-            this.soTkDataGridViewTextBoxColumn.DataPropertyName = "So_Tk";
-            this.soTkDataGridViewTextBoxColumn.HeaderText = "So_Tk";
-            this.soTkDataGridViewTextBoxColumn.Name = "soTkDataGridViewTextBoxColumn";
-            // 
-            // dataExcelBindingSource
-            // 
-            this.dataExcelBindingSource.DataSource = typeof(VietQR.Data_Excel);
-            // 
-            // bt_xuatFilePdf
-            // 
-            this.bt_xuatFilePdf.Location = new System.Drawing.Point(22, 91);
-            this.bt_xuatFilePdf.Name = "bt_xuatFilePdf";
-            this.bt_xuatFilePdf.Size = new System.Drawing.Size(113, 23);
-            this.bt_xuatFilePdf.TabIndex = 2;
-            this.bt_xuatFilePdf.Text = "Xuất File PDF";
-            this.bt_xuatFilePdf.UseVisualStyleBackColor = true;
-            this.bt_xuatFilePdf.Click += new System.EventHandler(this.bt_xuatFilePdf_Click);
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(19, 21);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(71, 13);
-            this.linkLabel1.TabIndex = 1;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Lấy Template";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // bt_LoadExcel
-            // 
-            this.bt_LoadExcel.Location = new System.Drawing.Point(22, 47);
-            this.bt_LoadExcel.Name = "bt_LoadExcel";
-            this.bt_LoadExcel.Size = new System.Drawing.Size(132, 23);
-            this.bt_LoadExcel.TabIndex = 0;
-            this.bt_LoadExcel.Text = "Load Template";
-            this.bt_LoadExcel.UseVisualStyleBackColor = true;
-            this.bt_LoadExcel.Click += new System.EventHandler(this.bt_LoadExcel_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "Excel File|*.xlsx";
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 402);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tạo Theo DS";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // statusStrip1
             // 
@@ -298,42 +257,45 @@ namespace VietQR
             this.statusStrip1.Location = new System.Drawing.Point(0, 428);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.TabIndex = 4;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // toolStripProgressBar1
+            // tabControl1
             // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
-            this.toolStripProgressBar1.Visible = false;
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 428);
+            this.tabControl1.TabIndex = 3;
             // 
-            // MainForm
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "MainForm";
-            this.Text = "Tạo File VietQR BIDV - tunn1@bidv.com.vn - Ver: 2.0";
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.tabControl1.ResumeLayout(false);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
             this.tabPage2.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataExcelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataExcelBindingSource)).EndInit();
+            this.tabPage1.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -341,31 +303,28 @@ namespace VietQR
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button bt_LoadExcel;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tieuDeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn hoTenDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn soTkDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource dataExcelBindingSource;
-        private System.Windows.Forms.Button bt_xuatFilePdf;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tb_tenchutk;
         private System.Windows.Forms.MaskedTextBox tb_sotk;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button bt_taoma;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tb_tieude;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button bt_xuatFilePdf;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button bt_LoadExcel;
+        private System.Windows.Forms.BindingSource dataExcelBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
